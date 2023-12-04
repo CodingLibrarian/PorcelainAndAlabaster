ad37332994ea73fdc1d1a75bdfcdc8c53ec3298a
John Gage <jgage1990@gmail.com>
2023-12-03 19:56:52 -0600

    Got jQuery into TypeScript, added npm, and being on ts for event carousel

---
+++ PorcelainAndAlabaster/TypeScript/HomeEvents.ts
@@ -0,0 +1,28 @@
+﻿function moveCarouselOne(moveForward: boolean) {
+    if (moveForward) {
+        let currentDot = $('.carousel-dots.active');
+        let currentItem = $('.carousel-item-view > div.active');
+        currentDot.removeClass('active');
+        currentItem.removeClass('active');
+        if (currentDot.next('li') !== null || currentDot.next('li') !== undefined) {
+            currentDot.next('li').addClass('active');
+            currentItem.next('div').addClass('active');
+        } else {
+            currentDot.parent('ul').children().first().addClass('active');
+            currentItem.parent('div').children().first().addClass('active');
+        }
+    } else {
+        let currentDot = $('.carousel-dots.active');
+        let currentItem = $('.carousel-item-view > div.active');
+        currentDot.removeClass('active');
+        currentItem.removeClass('active');
+        if (currentDot.prev('li') !== null || currentDot.prev('li') !== undefined) {
+            currentDot.prev('li').addClass('active');
+            currentItem.prev('div').addClass('active');
+        } else {
+            currentDot.parent('ul').children().last().addClass('active');
+            currentItem.parent('div').children().last().addClass('active');
+        }
+    }
+
+}
